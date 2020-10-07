7d276ccd0dd25d7e28286b728ab7b06f136f8ff1
Henric Edwards <62654315+HenricEdw@users.noreply.github.com>
2020-10-07 09:05:50 +0200

    Add files via upload

---
+++ IFileWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookInterfaceExample
+{
+    //Det finns många sätt för oss att spara saker till filer
+    //vi låtsas att det är precis det vi försöker göra här
+    interface IFileWriter
+    {
+        string Extension { get; }
+
+        string WriteToFile(string fileName);
+    }
+}
---
+++ TextFileWriter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookInterfaceExample
+{
+    class TextFileWriter : IFileWriter
+    {
+        public string Extension {  get { return ".txt"; } }
+
+        public string WriteToFile(string fileName)
+        {
+            //Vi fejkar att vi skriver ned till en textfil här
+            //och returnerar "sökvägen" till filen.
+            return $"Har skrivit {fileName + Extension}-fil till disk!";
+        }
+    }
+}
